f43b66b5d129ac5aaeac23f5e6c752299e0b1fd2
JesseVolstruis <schroder.jesse11@gmail.com>
2025-04-29 16:21:32 +0200

    Fixed code: Added comments for functions and changes, fixed calculations

--- bmi.cpp
+++ bmi.cpp
@@ -6,36 +6,54 @@
 
 using namespace std;
 
-int ounces2pounds(int x)
+//Converts ounces to pounds
+double ounces2pounds(int x)
 {
-    return(x*16);
+    // Jesse - Changed data type and to double
+    // Jesse - Changed from multiplying by 16 to dividing by 16.0
+    return(x/16.0);
 }
 
-int stones2pounds(int x)
+//Converts stones into pounds
+double stones2pounds(int x)
 {
-    return(x*14);
+    // Jesse - Changed data type and to double and 14 to 14.0
+    return(x*14.0);
 }
-
+//Accepts stones, ounces and pounds. Converts stones and ounces to pounds. Adds all 3. Converts to kilograms
 double weight2kg(int stones, int pounds, int ounces)
 {
     return (stones2pounds(stones)+pounds+ounces2pounds(ounces))/2.2;
 }
 
-double height2metres(int feet, int inches)
+//Converts inches to feet
+double inches2feet(int x)
 {
-    return(feet/3.82);
+    // Jesse - Added fucntion that accepts inches and converts it to feet
+    return (x/12.0);
 }
 
+//Takes in a height in feet and inches. Converts it to meters
+double height2metres(int feet, int inches)
+{
+    // Jesse - Changed 3.82 in return to 3.28 and added the inches converted to feet
+    return((feet+inches2feet(inches))/3.28);
+}
+
+//Takes in weight and height. Calculates and grades BMI
 char categorise(double kg, double metre)
 {
-    double bmi = kg*kg/metre;
+    // Jesse - Changed bmi from weight^2/height to weight/height^2
+    double bmi = kg/(metre*metre);
     char cat;
     if (bmi<19)
         cat='A';
-    else if (bmi<=26)
+    else if (bmi >=19 && bmi<25) 
         cat='B';
-    else if (bmi<=30)
+        // Jesse - changed <=26 to >=19 && <25
+    else if (bmi >=25 && bmi<30)
         cat='C';
+        // Jesse - changed <=30 to >=25 && <30
     else
         cat='D';
     return(cat);
@@ -55,7 +73,8 @@ void process_data(char* input_file, char* output_file)
     f_out.open(output_file,ofstream::out);
     while (!f_in.eof())
     {
-    	f_in >> person_id >> pounds >> stones >> ounces >> feet >> inches;
+        // Jesse - Swapped order of stones and pounds when reading from file
+    	f_in >> person_id >> stones >> pounds >> ounces >> feet >> inches;
         kg=weight2kg(int(stones),int(pounds),int(ounces));
         m =height2metres(int(feet),int(inches));
         cat=categorise(kg,m);
